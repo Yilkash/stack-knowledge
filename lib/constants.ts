6f50b76915a57d66957fead01fe50d1802cb7560
Yilkash <yilritdimka@gmail.com>
2026-02-11 05:16:31 +0100

    feat(config): add application constants and categories

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export const CATEGORIES = [
+  "Computer Science",
+  "Mathematics",
+  "Physics",
+  "Engineering",
+  "Medicine",
+  "Law",
+  "Business",
+  "Humanities"
+];
+
+export const APP_NAME = "StackKnowledge";
+export const APP_DESCRIPTION = "Decentralized Share-to-Earn Knowledge Platform";
