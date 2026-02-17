c8694c3f4e2ac4dc5fba194ec576e09f96df55da
Yilkash <yilritdimka@gmail.com>
2026-02-17 04:31:21 +0100

    feat: expand project constants with social and fee info

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -11,3 +11,14 @@ export const CATEGORIES = [
 
 export const APP_NAME = "StackKnowledge";
 export const APP_DESCRIPTION = "Decentralized Share-to-Earn Knowledge Platform";
+
+export const SOCIAL_LINKS = {
+  twitter: "https://twitter.com/stackknowledge",
+  github: "https://github.com/Yilkash/stack-knowledge",
+  discord: "https://discord.gg/stackknowledge",
+};
+
+export const CONTACT_EMAIL = "hello@stackknowledge.org";
+
+export const PLATFORM_FEE_PERCENTAGE = 5;
+export const MICROSTACKS_PER_STX = 1_000_000;
