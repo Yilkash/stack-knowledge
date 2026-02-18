7414478c1da6363786673a949ee4539cc964b64e
Yilkash <yilritdimka@gmail.com>
2026-02-18 11:04:38 +0100

    feat: add multi-step resource upload modal

--- frontend/components/UploadModal.tsx
+++ frontend/components/UploadModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Card from './Card';
 import Button from './Button';
 import { X, UploadCloud } from 'lucide-react';
