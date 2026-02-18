2a1000187486500204e168056a540dd76ccdd449
Yilkash <yilritdimka@gmail.com>
2026-02-18 11:04:38 +0100

    feat: implement STX tipping button with post-conditions

--- frontend/components/TipButton.tsx
+++ frontend/components/TipButton.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Button from './Button';
 import { useContract } from '@/hooks/use-contract';
 
