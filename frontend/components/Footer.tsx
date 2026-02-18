d14ef2af86a8f102f2490dfb6288754239ff1ba4
Yilkash <yilritdimka@gmail.com>
2026-02-18 11:03:58 +0100

    docs: add contact and about links to footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import React from 'react';
 import Container from './Container';
 import Logo from './Logo';
 import { SOCIAL_LINKS, APP_NAME } from '@/lib/constants';
