3b1521378bdcf1322adc03464e1471f17322e577
Yilkash <yilritdimka@gmail.com>
2026-02-18 11:03:00 +0100

    feat: add container card with subtle glassmorphism

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,8 +1,8 @@
+import { HTMLAttributes, ReactNode } from 'react';
 import { cn } from '@/lib/utils';
-import React from 'react';
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
-    children: React.ReactNode;
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+    children: ReactNode;
     className?: string;
 }
 
