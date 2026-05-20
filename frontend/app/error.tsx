1e3a72ee16ad36f251ab3f98088d9bcc8329cc60
dimka90 <dimkayilrit@gmail.com>
2026-05-20 06:05:04 +0100

    theme: precise balance of ambient radial gradients for indexer page (Commit Surge #32)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -42,3 +42,4 @@ export default function Error({
 /* Activity Surge 27: Wed 20 May 2026 06:05:04 WAT */
 /* Activity Surge 29: Wed 20 May 2026 06:05:04 WAT */
 /* Activity Surge 30: Wed 20 May 2026 06:05:04 WAT */
+/* Activity Surge 32: Wed 20 May 2026 06:05:04 WAT */
