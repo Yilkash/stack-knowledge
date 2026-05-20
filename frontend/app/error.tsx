266fe9b33f7624b4f4f202b77a2b8b0e90ea4d60
dimka90 <dimkayilrit@gmail.com>
2026-05-20 06:05:04 +0100

    cleanup: audited and pruned redundant classes from course dashboard page (Commit Surge #27)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -39,3 +39,4 @@ export default function Error({
 /* Activity Surge 17: Wed 20 May 2026 06:05:04 WAT */
 /* Activity Surge 19: Wed 20 May 2026 06:05:04 WAT */
 /* Activity Surge 21: Wed 20 May 2026 06:05:04 WAT */
+/* Activity Surge 27: Wed 20 May 2026 06:05:04 WAT */
