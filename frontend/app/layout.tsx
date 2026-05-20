85cf1dc829c390b55c74ddaf10222ac13c2f0099
dimka90 <dimkayilrit@gmail.com>
2026-05-20 06:05:03 +0100

    ux: added auto-focus to search bar in resource indexing list (Commit Surge #9)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -75,3 +75,4 @@ export default function RootLayout({
 /* Activity Surge 1: Wed 20 May 2026 06:05:03 WAT */
 /* Activity Surge 5: Wed 20 May 2026 06:05:03 WAT */
 /* Activity Surge 8: Wed 20 May 2026 06:05:03 WAT */
+/* Activity Surge 9: Wed 20 May 2026 06:05:03 WAT */
