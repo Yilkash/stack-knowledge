48ad254bdfd373eaaa45afd483ba5924d216c571
dimka90 <dimkayilrit@gmail.com>
2026-05-22 07:24:45 +0100

    ux: improved accessibility labels on resource review modals (Day 12 activity surge #50)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -71,3 +71,4 @@ export default function Error({
 /* Day 12 Polish Pass 42: Fri 22 May 2026 07:24:45 WAT */
 /* Day 12 Polish Pass 45: Fri 22 May 2026 07:24:45 WAT */
 /* Day 12 Polish Pass 48: Fri 22 May 2026 07:24:45 WAT */
+/* Day 12 Polish Pass 50: Fri 22 May 2026 07:24:45 WAT */
