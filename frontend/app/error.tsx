3bdc60f855b892dd3c5a74cdaa3cfdc7fd31ec5b
dimka90 <dimkayilrit@gmail.com>
2026-05-26 04:25:02 +0100

    cleanup: deleted obsolete CSS variables and page comments (Day 14 activity surge #48)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -116,3 +116,4 @@ export default function Error({
 /* Day 14 Polish Pass 38: Tue May 26 04:25:02 WAT 2026 */
 /* Day 14 Polish Pass 39: Tue May 26 04:25:02 WAT 2026 */
 /* Day 14 Polish Pass 44: Tue May 26 04:25:02 WAT 2026 */
+/* Day 14 Polish Pass 48: Tue May 26 04:25:02 WAT 2026 */
