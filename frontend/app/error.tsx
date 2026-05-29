9e40b7fb8efe924b25b19f539cb16cb8822ed80e
dimka90 <dimkayilrit@gmail.com>
2026-05-29 05:29:09 +0100

    polish: tightened spacing in lesson preview cards (Day 18 activity surge #50)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -155,3 +155,4 @@ export default function Error({
 /* Day 18 Polish Pass 34: Fri 29 May 2026 05:29:09 WAT */
 /* Day 18 Polish Pass 39: Fri 29 May 2026 05:29:09 WAT */
 /* Day 18 Polish Pass 42: Fri 29 May 2026 05:29:09 WAT */
+/* Day 18 Polish Pass 50: Fri 29 May 2026 05:29:09 WAT */
