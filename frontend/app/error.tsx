c28e0eb5bb8357bb329d52ffbed5a5e913109fcc
dimka90 <dimkayilrit@gmail.com>
2026-05-29 05:29:09 +0100

    refactor: extracted download badge into shared component (Day 18 activity surge #30)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -150,3 +150,4 @@ export default function Error({
 /* Day 18 Polish Pass 24: Fri 29 May 2026 05:29:09 WAT */
 /* Day 18 Polish Pass 26: Fri 29 May 2026 05:29:09 WAT */
 /* Day 18 Polish Pass 27: Fri 29 May 2026 05:29:09 WAT */
+/* Day 18 Polish Pass 30: Fri 29 May 2026 05:29:09 WAT */
