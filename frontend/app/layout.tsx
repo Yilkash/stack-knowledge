4b5fd6b59516485172a4b66295ed6d7728f4fafa
dimka90 <dimkayilrit@gmail.com>
2026-05-29 05:29:08 +0100

    polish: tightened spacing in lesson preview cards (Day 18 activity surge #1)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -170,3 +170,4 @@ export default function RootLayout({
 /* Day 14 Polish Pass 42: Wed May 27 05:24:44 WAT 2026 */
 /* Day 14 Polish Pass 44: Wed May 27 05:24:44 WAT 2026 */
 /* Day 14 Polish Pass 46: Wed May 27 05:24:44 WAT 2026 */
+/* Day 18 Polish Pass 1: Fri 29 May 2026 05:29:08 WAT */
