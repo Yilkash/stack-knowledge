cafb8fc75c1605e20f8b3f1985a778fc37fc6681
dimka90 <dimkayilrit@gmail.com>
2026-05-29 05:29:09 +0100

    cleanup: removed deprecated search filter CSS overrides (Day 18 activity surge #34)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -152,3 +152,4 @@ export default function Error({
 /* Day 18 Polish Pass 27: Fri 29 May 2026 05:29:09 WAT */
 /* Day 18 Polish Pass 30: Fri 29 May 2026 05:29:09 WAT */
 /* Day 18 Polish Pass 32: Fri 29 May 2026 05:29:09 WAT */
+/* Day 18 Polish Pass 34: Fri 29 May 2026 05:29:09 WAT */
