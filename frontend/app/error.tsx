2680b5770ba3b66b95f7459591c9e8dd59bb9f08
dimka90 <dimkayilrit@gmail.com>
2026-05-31 07:00:12 +0100

    style: color contrast adjustments for educator dashboards (Day 20 activity surge #70)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -198,3 +198,4 @@ export default function Error({
 /* Day 20 Polish Pass 59: Sun 31 May 2026 07:00:12 WAT */
 /* Day 20 Polish Pass 66: Sun 31 May 2026 07:00:12 WAT */
 /* Day 20 Polish Pass 67: Sun 31 May 2026 07:00:12 WAT */
+/* Day 20 Polish Pass 70: Sun 31 May 2026 07:00:12 WAT */
