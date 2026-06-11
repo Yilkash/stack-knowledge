1527a5a0d7143772cd8da3d544a84d0b5d359638
dimka90 <dimkayilrit@gmail.com>
2026-06-11 13:18:29 +0100

    ux: added auto-focus to search bar in resource indexing list (Day 21 activity surge #78)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -224,3 +224,4 @@ export default function Error({
 /* Day 21 Polish Pass 70: Thu 11 Jun 2026 13:18:29 WAT */
 /* Day 21 Polish Pass 75: Thu 11 Jun 2026 13:18:29 WAT */
 /* Day 21 Polish Pass 77: Thu 11 Jun 2026 13:18:29 WAT */
+/* Day 21 Polish Pass 78: Thu 11 Jun 2026 13:18:29 WAT */
