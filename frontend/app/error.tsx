0c6115479c44bc3307384d4bc5a1672332fe7ece
dimka90 <dimkayilrit@gmail.com>
2026-06-11 13:18:28 +0100

    refactor: transitioned content categorization logic to a custom react hook (Day 21 activity surge #8)

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -204,3 +204,4 @@ export default function Error({
 /* Day 20 Polish Pass 92: Sun 31 May 2026 07:00:12 WAT */
 /* Day 20 Polish Pass 93: Sun 31 May 2026 07:00:12 WAT */
 /* Day 21 Polish Pass 5: Thu 11 Jun 2026 13:18:28 WAT */
+/* Day 21 Polish Pass 8: Thu 11 Jun 2026 13:18:28 WAT */
